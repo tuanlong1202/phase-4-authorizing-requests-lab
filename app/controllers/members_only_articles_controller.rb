7ff06d4264ead3bc3e7a77397618b199fbdd2271
tuanlong1202 <tuanlong1202@gmail.com>
2021-09-23 01:11:36 -0400

    Phase 4 authorizing request lab

--- app/controllers/members_only_articles_controller.rb
+++ app/controllers/members_only_articles_controller.rb
@@ -1,4 +1,5 @@
 class MembersOnlyArticlesController < ApplicationController
+  before_action :authorize
   rescue_from ActiveRecord::RecordNotFound, with: :record_not_found
 
   def index
@@ -7,7 +8,7 @@ class MembersOnlyArticlesController < ApplicationController
   end
 
   def show
-    article = Article.find(params[:id])
+    article = Article.find(params[:id]).where(is_member_only: true)
     render json: article
   end
 
@@ -16,5 +17,8 @@ class MembersOnlyArticlesController < ApplicationController
   def record_not_found
     render json: { error: "Article not found" }, status: :not_found
   end
-
+  
+  def authorize
+    return render json: { error: "Not authorized" }, status: :unauthorized unless session.include? :user_id
+  end
 end
